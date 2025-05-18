618602e4cf9344adda7b35f6e2e65247f5e9f1e3
Clifton <clifton3833@gmail.com>
2025-05-18 00:48:10 -0700

    Coded base game

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -16,6 +17,10 @@ SCREEN_HEIGHT = 600
 player_x = 340
 ball_x=SCREEN_WIDTH/2
 ball_y = SCREEN_HEIGHT - 40
+ball_x_direction = 0
+ball_y_direction = 0
+ball_x_speed = 4
+ball_y_speed = 4
 board = [[5,5,5,5,5,5,5,5], [4,4,4,4,4,4,4,4], [3,3,3,3,3,3,3,3], [2,2,2,2,2,2,2,2], [1,1,1,1,1,1,1,1]]
 colors = [red, orange, green, blue, purple]
 speed = 8
@@ -27,15 +32,23 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 #Sets up the board
 def draw_board(board):
+    board_squares = []
     for i in range(len(board)):
         for j in range(len(board[i])):
-            pygame.draw.rect(screen, colors[(board[i][j])-1], [j*100, i*40, 98 ,38])
+            if board[i][j] > 0:   
+                piece = pygame.draw.rect(screen, colors[(board[i][j])-1], [j*100, i*40, 98 ,38])
+                top = pygame.rect.Rect((j*100, i*40), (98 ,1))
+                bot = pygame.rect.Rect((j*100, (i*40) +37 ),(98 ,1))
+                left = pygame.rect.Rect((j*100, i*40), (37 ,1))
+                right = pygame.rect.Rect(((j*100)+97, i*40), ( 37,1))
+                board_squares.append([top, bot, left, right, (i,j)])
+    return board_squares
 
 run = True
 while run:
     screen.fill(black)
     timer.tick(fps)
-    draw_board(board)
+    squares = draw_board(board)
 
     player = pygame.draw.rect(screen, red, [player_x, SCREEN_HEIGHT - 30, 120, 15], 0)
     ball = pygame.draw.circle(screen, white,[ball_x,ball_y], 10)
@@ -46,16 +59,39 @@ while run:
 
 #Buttons to move left right
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_SPACE]:
+    if keys[pygame.K_SPACE] and not active:
         active = True
-    elif keys[pygame.K_a]:
+        ball_y_direction = -1
+        ball_x_direction = random.choice([-1,1])
+    elif keys[pygame.K_a] and active:
         player_direction = -1
-    elif keys[pygame.K_d]:
+    elif keys[pygame.K_d]  and active:
         player_direction = 1
     else:
         player_direction = 0
 
+    if ball_x <= 10 or ball_x >= SCREEN_WIDTH -10:
+        ball_x_direction *= -1
+
+    if ball.colliderect(player):
+        ball_y_direction *= -1
+
+    for i in range(len(squares)):
+        if ball.colliderect(squares[i][0]) or ball.colliderect(squares[i][1]):
+            ball_y_direction *= -1 
+            board[squares[i][4][0]][squares[i][4][1]]-=1
+        if ball.colliderect(squares[i][2]) or ball.colliderect(squares[i][3]):
+            ball_x_direction *= -1 
+            board[squares[i][4][0]][squares[i][4][1]]-=1
+        
+        
+    if ball_y >= SCREEN_HEIGHT - 10:
+        active = False
+
+  
     player_x += player_direction*speed
+    ball_y += ball_y_direction * ball_y_speed
+    ball_x += ball_x_direction * ball_x_speed
 
     pygame.display.flip()
 
